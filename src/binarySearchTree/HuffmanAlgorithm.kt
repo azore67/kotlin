a3a6dff7baa54d4e96baad99b813e0a670a7c3f0
Atharva <atharva.zore2005@gmail.com>
2024-04-10 16:43:32 +0100

    huffman

---
+++ src/binarySearchTree/HuffmanAlgorithm.kt
@@ -0,0 +1,23 @@
+package binarySearchTree
+
+data class TN(val char: Char, val frequency: Int, var left: TN? = null, var right: TN? = null)
+
+class BST{
+    private var root: TN? = null
+
+    fun buildHuffmanTree(characters: Map<Char,Int>): TN {
+        val nodes = characters.map {(char,frequency) -> TN(char,frequency) }.toMutableList()
+        while (nodes.size > 1) {
+            nodes.sortBy{it.frequency}
+            val leftNode = nodes.removeAt(0)
+            val rightNode = nodes.removeAt(0)
+            val mergedNode = TN('.', leftNode.frequency + rightNode.frequency)
+            mergedNode.left = leftNode
+            mergedNode.right = rightNode
+
+        }
+        return nodes.first()
+    }
+
+}
+
